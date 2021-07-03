2135d9a44979be4b5ab84ad2c21143936278c5a3
swillrich5 <swillrich@gmail.com>
2021-07-03 08:27:38 -0500

    initial setup for pagination refactoring

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import SearchCharacters from './pages/SearchCharacters';
+// import Episode from './pages/Episode';
+import Character from './pages/Character';
+import './App.css';
+
+
+function App() {
+  return (
+    <Router>  
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/characters' component={SearchCharacters}/>  
+          <Route path='/character/:id' component={Character} />        
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
