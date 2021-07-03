0e46a80c6d11105a81680569f626da430de2b9d3
swillrich5 <swillrich@gmail.com>
2021-07-03 10:45:29 -0500

    added spinner when loading to initial get character api call

--- src/components/CharacterResults.js
+++ src/components/CharacterResults.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import md5 from 'js-md5';
+import Spinner from './Spinner';
 
 
 const CharacterResults = ({ characterName }) => {
@@ -43,26 +44,28 @@ const CharacterResults = ({ characterName }) => {
         getCharacters();
     }, [characterName]);
 
-    
-
-    return (
-        <div className="row">
-        {characters.map(character =>
-            <div key={character.id} className='col-lg-4 col-md-6 col-sm-12 justify-content-around'>
-                <div className="card mb-3">
-                    <div className="card-body">
-                        <div className='row'>
-                            <img className="pl-4 col-5" src={character.thumbnail.path + '/portrait_small.jpg'} alt="" />
-                            <h5 className="card-title col-7 mt-2 pl-0">{character.name}</h5>
+    if (loading) {
+        return <Spinner />
+    } else {
+        return (
+            <div className="row">
+            {characters.map(character =>
+                <div key={character.id} className='col-lg-4 col-md-6 col-sm-12 justify-content-around'>
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <div className='row'>
+                                <img className="pl-4 col-5" src={character.thumbnail.path + '/portrait_small.jpg'} alt="" />
+                                <h5 className="card-title col-7 mt-2 pl-0">{character.name}</h5>
+                            </div>
+                            {/* <p className="card-subtitle scroll mb-2">{character.description}</p> */}
+                            {/* <Link to={`/character/${character.id}`} className='btn btn-dark btn-sm my-2'>More</Link> */}
                         </div>
-                        {/* <p className="card-subtitle scroll mb-2">{character.description}</p> */}
-                        {/* <Link to={`/character/${character.id}`} className='btn btn-dark btn-sm my-2'>More</Link> */}
                     </div>
                 </div>
-            </div>
-        )}
-    </div>
-    )
+            )}
+        </div>
+        )
+    }
 }
 
 export default CharacterResults
