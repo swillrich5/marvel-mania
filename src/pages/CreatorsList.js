fb3330f77cbe1a06523eaaf7300f5ba97bea07f3
swillrich5 <swillrich@gmail.com>
2021-07-19 06:28:09 -0500

    added definition of comic creator to CreatorsList.js

--- src/pages/CreatorsList.js
+++ src/pages/CreatorsList.js
@@ -7,6 +7,10 @@ const CreatorsList = () => {
             <div className="row d-block justify-content-center">
                 <h2 className="pb-2">Marvel Comics Creators</h2>
                 <p className="lead text-center">Content Coming Soon!</p>
+                <p className="lead text-justify px-5">
+                    Comic Book creators are the incredibly talented people that write the stories and create the artwork that goes into each 
+                    comic book.  Some of the legendary creators are Jack Kirby, John Byrne, Frank Miller, Art Adams, Jim Lee, and Dave Sim.
+                </p>
             </div>
         </div>
     </div>
