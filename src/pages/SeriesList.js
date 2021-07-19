8fd508a8f62bfa43506146fb95b2c2402af8d64f
swillrich5 <swillrich@gmail.com>
2021-07-19 05:35:32 -0500

    added definition of comic book series to SeriesList.js

--- src/pages/SeriesList.js
+++ src/pages/SeriesList.js
@@ -7,6 +7,13 @@ const SeriesList = () => {
             <div className="row d-block justify-content-center">
                 <h2 className="pb-2">Marvel Comics Series</h2>
                 <p className="lead text-center">Content Coming Soon!</p>
+                <p className="mx-5">A comic book series is a run of comic books that is set to run for a certain number of issues or indefinitely.  
+                   The term can be a little confusing.  A series is also sometimes called a title, which refers to the entire series, 
+                   not a single, discrete unit.  <b>Ongoing Series</b> are series that have no definite end date.  <b>Mini-Series</b> are
+                   comic book series that are scheduled to run only a certain number of issues, usually six or fewer, and then 
+                   end.  <b>Maxi-Series</b> run more than six issues and then end.  Mini-series and maxi-series can both 
+                   be referred to as <b>Limited Series</b>.
+                </p>
             </div>
         </div>
     </div>
