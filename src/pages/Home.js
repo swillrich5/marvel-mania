5598be38cf29899682d150980e051dc03c027b2a
swillrich5 <swillrich@gmail.com>
2021-07-20 10:06:00 -0500

    updated Home.js intro wording

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,9 +13,9 @@ const Home = () => {
             <div className="jumbotron">
                 <h2>Welcome to Marvel-API-Browser</h2>
                 <h2>Learning More about the Marvel API</h2>
-                <p className="lead">This is my attempt at exploring Marvel Comics' API (Application Programming Interface) to learn more about the API and APIs in general.  I'm also hopeful that this will help others to learn more about Marvel and how their API works.
-                    This is a work in progress that I plan to continue to add to over time.  However, it's far enough along to be useful.  I hope this is helpful 
-                    and maybe a little fun!
+                <p className="lead">This is my attempt at exploring Marvel Comics' API (Application Programming Interface) to learn more about the API and APIs in general.  
+                    I'm also using this as an exercise to learn more about React.  This is a work in progress that I plan to continue to add to over time.  However, 
+                    it's far enough along to be useful.  I hope this is helpful and maybe a little fun!
                 </p>
                 <p className="lead mt-3 mb-5">My code repo for this app can be found in my <a  className="text-primary" href="https://github.com/swillrich5/marvel-mania">GitHub</a>.  Also, here's a link to my <a className="text-primary" href="https://www.scottwillrich.com/">portfolio site</a>.</p>
                 <div className="row justify-content-center">
